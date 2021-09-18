a3e0b79ff05d8e610d1c8ae5165dec423d443fdb
Anderson Toshiyuki Sasaki <anderson.sasaki@gmail.com>
2021-09-18 19:00:43 +0200

    Add app entrypoints (scripts) to package
    Add the 'start' and 'test' entrypoints to the app package.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <anderson.sasaki@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const PORT = process.env.PORT || 3000
 const express = require('express');
-const app = express();
+const path = require('path')
 const http = require('http');
+const app = express()
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+       res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
@@ -18,6 +20,5 @@ io.on('connection', (socket) => {
 	});
 });
 
-server.listen(3000, () => {
-	console.log('listening on *:3000');
-});
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
